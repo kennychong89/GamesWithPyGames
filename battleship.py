aa57990f9a4188491b6eb4cf89a5adfe3a965896
Kenny Chong <kennychong89@gmail.com>
2013-12-03 20:33:46 +0000

    Added highlighting so that you now know where your ship will be placed on the board. Added a ship piece called 'BOAT', which covers 2 by 2 cell location and can be placed on board.

--- battleship.py
+++ battleship.py
@@ -1,4 +1,4 @@
-# A test board that I will experiment with for my battleship game.
+# Another test board using rectangle objects instead of lines
 import random, pygame, sys
 from pygame.locals import *
 
@@ -9,86 +9,171 @@ CELLSIZE = 20
 EMPTY = None
 
 assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
-assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size"
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
 
 CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
 CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
 
-WHITE =     (255, 255, 255)
-BLACK =     (  0,   0,   0)
-RED   =     (255,   0,   0)
-GREEN =     (  0, 255,   0)
-DARKGREEN = (  0, 155,   0)
-DARKGRAY  = ( 40,  40,  40)
-BGCOLOR   = BLACK
+WHITE = 	(255,255,255)
+BLACK = 	(  0,  0,  0)
+RED   = 	(255,  0,  0)
+GREEN = 	(  0,255,  0)
+DARKGREEN = 	(  0,155,  0)
+DARKGRAY  =     ( 40, 40, 40)
+BLUE      =     (  0,  0,255)
+BGCOLOR = BLACK
+HIGHTLIGHTCOLOR = BLUE
+
+# Types of ships
+BOAT = {'ROW': 2, 'COLUMN':2, 'COLOR':RED, 'NAME':'Boat'}
+
+PIECES = (BOAT)
 
 def main():
 	global DISPLAYSURF, BASICFONT
 
 	pygame.init()
 	DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-	BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+	BASICFONT   = pygame.font.Font('freesansbold.ttf', 18)
 	pygame.display.set_caption('Battleship')
 
 	while True:
 		runGame()
-		
+
 def runGame():
 	mousex = 0
 	mousey = 0
 
 	board = initBoard()
-
+	
+	#drawBoard()
+	#drawGrid()
+	
 	while True:
+		DISPLAYSURF.fill(BLUE)	
 		mouseClicked = False
-
+		
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				terminate()
-			elif event.type == MOUSEMOTION:
+			if event.type == MOUSEMOTION:
+				mousex, mousey = event.pos		
+			if event.type == MOUSEBUTTONUP:
 				mousex, mousey = event.pos
-			elif event.type == MOUSEBUTTONUP:
-				mousex, mousey = event.pos	
 				mouseClicked = True
-
-		gridx, gridy = convertPixelToGridCoord(mousex, mousey)
 	
-		# insert your logic here
-		# check if that cell is not already occupied
-		location  = retrieveCellInfo(board, gridx, gridy)		
-
-		if location == EMPTY and mouseClicked:
-			# add a square piece
-			addPieceToBoard(board, gridx, gridy)		 
-
-			# draw the piece	
-			drawPiece(board[gridx][gridy], gridx, gridy)
+		gridx, gridy = convertPixelToGridCoord(mousex, mousey)
+		
+		location = retrieveCellInfo(board, gridx, gridy)
+		
+		if location == EMPTY:
+			if mouseClicked:
+				# add a boat piece
+				addPieceToBoard(board, PIECES, gridx, gridy) 
+				
+			highlightCell(PIECES, gridx, gridy)
 			
 		drawGrid()
+		updateBoardInfo(board)
 		pygame.display.update()
+		
+		unhighlightCell(PIECES, gridx, gridy)
+
+def updateBoardInfo(board):
+	for x in range(0, CELLWIDTH):
+		for y in range(0, CELLHEIGHT):
+			if board[x][y] != EMPTY:
+				# retrieve ship at x,y
+				ship = board[x][y]
+		
+				# retrieve ship color at x,y
+				color = ship['COLOR']
+			
+				(pixelx, pixely) = convertGridToPixelCoord(x, y)
+			
+				# draw the ship into the surface
+				cell = pygame.Rect(pixelx, pixely, CELLSIZE, CELLSIZE)
+
+				pygame.draw.rect(DISPLAYSURF, color, cell)
+
+def checkBoardLocation(board, shipPiece, gridx, gridy):
+	# check if board location is occupied by another ship
+	pieceRow = shipPiece['ROW']
+
+	pieceColumn = shipPiece['COLUMN']
+
+	# out of bounds checkings
+	if (gridx + 1) - pieceRow < 0 or (gridy + 1) - pieceColumn < 0:
+		return False
+
+	# cell occupied Checkings
+	for x in range(gridx, gridx - pieceRow, -1):
+		for y in range(gridy, gridy - pieceColumn, -1):
+			if board[x][y] != None:
+				return False
+	return True
+
+def addPieceToBoard(board, shipPiece, gridx, gridy):
+	# add it to board at (x,y)
+
+	# retrieve # of rows
+	pieceRow = shipPiece['ROW']
+
+	# retrieve # of columns
+	pieceColumn = shipPiece['COLUMN']
+
+	# valid placement
+	err = checkBoardLocation(board, shipPiece, gridx, gridy)
+
+	if (err):
+		for x in range(gridx, gridx - pieceRow, -1):
+			for y in range(gridy, gridy - pieceColumn, -1):
+				board[x][y] = shipPiece
+
+def highlightCell(shipPiece, gridx, gridy):
+	pieceRow = shipPiece['ROW']
+
+	pieceColumn = shipPiece['COLUMN']	
+
+	for x in range(gridx, gridx - pieceRow, -1):
+		for y in range(gridy, gridy - pieceColumn, -1):
+			pixelx, pixely = convertGridToPixelCoord(x, y)
+
+			cell = pygame.Rect(pixelx, pixely, CELLSIZE, CELLSIZE)
+
+			pygame.draw.rect(DISPLAYSURF, RED, cell)	
+
+def unhighlightCell(shipPiece, gridx, gridy):
+	pieceRow = shipPiece['ROW']
+
+	pieceColumn = shipPiece['COLUMN']	
+
+	for x in range(gridx, gridx - pieceRow, -1):
+		for y in range(gridy, gridy - pieceColumn, -1):
+			pixelx, pixely = convertGridToPixelCoord(x, y)
+
+			cell = pygame.Rect(pixelx, pixely, CELLSIZE, CELLSIZE)
+
+			pygame.draw.rect(DISPLAYSURF, BLUE, cell)	
+
+def retrieveCellInfo(board, x, y):
+	return board[x][y]
+
+def initBoard():
+	board = []
 	
-def drawPiece(piece, gridx, gridy):
-	# convert it to pixel coord
-	pixelx, pixely = convertGridToPixelCoord(gridx, gridy)
+	for x in range(CELLWIDTH):
+		column = []
+		for y in range(CELLHEIGHT):
+			column.append(EMPTY)
 
-	cell = pygame.Rect(pixelx, pixely, CELLSIZE, CELLSIZE)
+		board.append(column)
 
-	pygame.draw.rect(DISPLAYSURF, piece, cell) 
-
-def drawGrid():
-	for x in range(0, WINDOWWIDTH, CELLSIZE):	# draw vertical line
-		pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
-	
-	for y in range(0, WINDOWHEIGHT, CELLSIZE):
-		pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
-
-def checkForKeyPress():
-	if len(pygame.event.get(QUIT)) > 0:
-		terminate()
+	return board
 
 def convertPixelToGridCoord(pixelx, pixely):
 	# Convert pixel coordinates to grid coordinates
-	left = int(pixelx / CELLSIZE)
+	left =  int(pixelx / CELLSIZE)
 
 	top = int(pixely / CELLSIZE)
 
@@ -102,31 +187,27 @@ def convertGridToPixelCoord(gridx, gridy):
 
 	return (left, top)
 
-def retrieveCellInfo(board, x, y): 
-	return board[x][y]
+def drawBoard():
+	for x in range(0, WINDOWWIDTH):
+		for y in range(0, WINDOWHEIGHT):
+			pixelx, pixely = convertGridToPixelCoord(x, y)
 
-def addPieceToBoard(board, x, y):
-	# create a piece tuple with a color
-	piece = (DARKGRAY)
+			# Create a cell
+			cell = pygame.Rect(pixelx, pixely, CELLSIZE, CELLSIZE)
+			
+			# Create  a border
+			pygame.draw.rect(DISPLAYSURF, BLUE, cell)
 
-	# add it to board at (x,y)
-	board[x][y] = piece
+def drawGrid():
+	for x in range(0, WINDOWWIDTH, CELLSIZE):
+		pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
 
-def initBoard():
-	board = []
-
-	for x in range(CELLWIDTH):
-		column = []
-		for y in range(CELLHEIGHT):
-			column.append(EMPTY)
-		
-		board.append(column)
-
-	return board
+	for y in range(0, WINDOWHEIGHT, CELLSIZE):
+		pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
 
 def terminate():
 	pygame.quit()
 	sys.exit()
 
-if __name__== '__main__':
-	main()	
+if __name__ == '__main__':
+	main()
